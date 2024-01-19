4815182e3bfa0156884fd8dd08d4a1e6a2943a89
Vinicius Rech <vinitrech@gmail.com.com.br>
2024-01-19 11:55:20 -0300

    implementing base auth api

---
+++ aspnet8-microservices/Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthAPI.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }    
+    }
+}
